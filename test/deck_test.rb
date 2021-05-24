fbe59b13a1acb79fea4abe7979bc3c5a277d457f
Khoi Nguyen <khoinguyen311@gmail.com>
2021-05-23 20:31:11 -0700

    Add test to check counting cards

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -28,4 +28,16 @@ class DeckTest < Minitest::Test
       assert_instance_of(Card, deck.cards[0])
     end
   end
+
+
+    def test_it_can_count_number_of_cards_in_deck
+      card_1 = Card.new("3", "Hearts")
+      card_2 = Card.new("4", "Clubs")
+      card_3 = Card.new("5", "Diamonds")
+      cards = [card_1, card_2, card_3]
+
+      deck = Deck.new(cards)
+
+      assert_equal(3, deck.count)
+    end
 end
