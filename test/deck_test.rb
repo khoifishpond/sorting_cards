a68ccc72f3579c7fb49d89c3a25cf48be622fe0e
Khoi Nguyen <khoinguyen311@gmail.com>
2021-05-23 20:35:36 -0700

    Correct body of test and fix indentation

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -16,17 +16,15 @@ class DeckTest < Minitest::Test
   end
 
   def test_it_takes_an_array_of_cards
-    def test_it_exists
-      card_1 = Card.new("3", "Hearts")
-      card_2 = Card.new("4", "Clubs")
-      card_3 = Card.new("5", "Diamonds")
-      cards = [card_1, card_2, card_3]
+    card_1 = Card.new("3", "Hearts")
+    card_2 = Card.new("4", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    cards = [card_1, card_2, card_3]
 
-      deck = Deck.new(cards)
+    deck = Deck.new(cards)
 
-      assert_instance_of(Array, deck.cards)
-      assert_instance_of(Card, deck.cards[0])
-    end
+    assert_instance_of(Array, deck.cards)
+    assert_instance_of(Card, deck.cards[0])
   end
 
   def test_it_can_count_number_of_cards_in_deck
