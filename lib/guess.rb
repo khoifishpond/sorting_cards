25f6f05f609c3fe88557536005eab980062792f3
Khoi Nguyen <khoinguyen311@gmail.com>
2021-05-23 17:15:14 -0700

    Add response and correct? methods

--- lib/guess.rb
+++ lib/guess.rb
@@ -5,4 +5,13 @@ class Guess
     @guess = guess
     @card = card
   end
+
+  def response # why does this even need to exist?
+    @guess     # literally already initialized
+  end
+
+  def correct?
+    response == "#{card.value} of #{card.suit}"
+  end
+
 end
