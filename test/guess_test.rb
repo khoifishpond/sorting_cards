cbc89d7f76dfbbbe7760c96cd1cac700fd6b5c2d
Khoi Nguyen <khoinguyen311@gmail.com>
2021-05-24 12:20:24 -0700

    Update test method

--- test/guess_test.rb
+++ test/guess_test.rb
@@ -22,7 +22,7 @@ class GuessTest < Minitest::Test
     card = Card.new("10", "Hearts")
     guess = Guess.new("10 of Hearts", card)
 
-    assert_equal("10 of Hearts", guess.response)
+    assert_equal("10 of Hearts", guess.guess)
   end
 
   def test_it_guess_is_correct
