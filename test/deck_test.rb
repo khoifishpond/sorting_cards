d5e2e682ac36d8bd2bda7714161ec2832ee63738
Khoi Nguyen <khoinguyen311@gmail.com>
2021-05-23 20:33:38 -0700

    Correct line spacing and indentation

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -29,15 +29,14 @@ class DeckTest < Minitest::Test
     end
   end
 
+  def test_it_can_count_number_of_cards_in_deck
+    card_1 = Card.new("3", "Hearts")
+    card_2 = Card.new("4", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    cards = [card_1, card_2, card_3]
 
-    def test_it_can_count_number_of_cards_in_deck
-      card_1 = Card.new("3", "Hearts")
-      card_2 = Card.new("4", "Clubs")
-      card_3 = Card.new("5", "Diamonds")
-      cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
 
-      deck = Deck.new(cards)
-
-      assert_equal(3, deck.count)
-    end
+    assert_equal(3, deck.count)
+  end
 end
