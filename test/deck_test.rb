5192b62f2033eb25b2bed024f885e6ca3a169685
Khoi Nguyen <khoinguyen311@gmail.com>
2021-05-23 20:30:05 -0700

    Add argument acceptance test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -14,4 +14,18 @@ class DeckTest < Minitest::Test
 
     assert_instance_of(Deck, deck)
   end
+
+  def test_it_takes_an_array_of_cards
+    def test_it_exists
+      card_1 = Card.new("3", "Hearts")
+      card_2 = Card.new("4", "Clubs")
+      card_3 = Card.new("5", "Diamonds")
+      cards = [card_1, card_2, card_3]
+
+      deck = Deck.new(cards)
+
+      assert_instance_of(Array, deck.cards)
+      assert_instance_of(Card, deck.cards[0])
+    end
+  end
 end
