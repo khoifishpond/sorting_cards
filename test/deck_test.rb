87d5841f1469dc083c21bbacb41ee25006ea0d77
Khoi Nguyen <khoinguyen311@gmail.com>
2021-05-23 20:21:53 -0700

    Add deck testing

---
+++ test/deck_test.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/deck'
+require './lib/card'
+
+class DeckTest < Minitest::Test
+  def test_it_exists
+    card_1 = Card.new("3", "Hearts")
+    card_2 = Card.new("4", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    cards = [card_1, card_2, card_3]
+
+    deck = Deck.new(cards)
+
+    assert_instance_of(Deck, deck)
+  end
+end
