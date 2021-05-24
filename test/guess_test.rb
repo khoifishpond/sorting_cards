4982f0fdbec7d3c3504fa5c7c682721a7a58dfdb
Khoi Nguyen <khoinguyen311@gmail.com>
2021-05-23 17:15:28 -0700

    Add correctness testing

--- test/guess_test.rb
+++ test/guess_test.rb
@@ -22,6 +22,20 @@ class GuessTest < Minitest::Test
     card = Card.new("10", "Hearts")
     guess = Guess.new("10 of Hearts", card)
 
-    assert_equal("10 of Hearts", guess.guess)
+    assert_equal("10 of Hearts", guess.response)
+  end
+
+  def test_it_guess_is_correct
+    card = Card.new("10", "Hearts")
+    guess = Guess.new("10 of Hearts", card)
+
+    assert(guess.correct?)
+  end
+
+  def test_it_guess_can_be_incorrect
+    card = Card.new("10", "Hearts")
+    guess = Guess.new("2 of Diamonds", card)
+
+    refute(guess.correct?)
   end
 end
