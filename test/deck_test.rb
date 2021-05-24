86c2ac84606402b539d0e446cfb7fc13b1c7af96
Khoi Nguyen <khoinguyen311@gmail.com>
2021-05-23 20:43:52 -0700

    Add setup method and update variables

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -4,37 +4,25 @@ require './lib/deck'
 require './lib/card'
 
 class DeckTest < Minitest::Test
+  def setup
+    @card_1 = Card.new("3", "Hearts")
+    @card_2 = Card.new("4", "Clubs")
+    @card_3 = Card.new("5", "Diamonds")
+    @cards = [@card_1, @card_2, @card_3]
+
+    @deck = Deck.new(@cards)
+  end
+
   def test_it_exists
-    card_1 = Card.new("3", "Hearts")
-    card_2 = Card.new("4", "Clubs")
-    card_3 = Card.new("5", "Diamonds")
-    cards = [card_1, card_2, card_3]
-
-    deck = Deck.new(cards)
-
-    assert_instance_of(Deck, deck)
+    assert_instance_of(Deck, @deck)
   end
 
   def test_it_takes_an_array_of_cards
-    card_1 = Card.new("3", "Hearts")
-    card_2 = Card.new("4", "Clubs")
-    card_3 = Card.new("5", "Diamonds")
-    cards = [card_1, card_2, card_3]
-
-    deck = Deck.new(cards)
-
-    assert_instance_of(Array, deck.cards)
-    assert_instance_of(Card, deck.cards[0])
+    assert_instance_of(Array, @deck.cards)
+    assert_instance_of(Card, @deck.cards[0])
   end
 
   def test_it_can_count_number_of_cards_in_deck
-    card_1 = Card.new("3", "Hearts")
-    card_2 = Card.new("4", "Clubs")
-    card_3 = Card.new("5", "Diamonds")
-    cards = [card_1, card_2, card_3]
-
-    deck = Deck.new(cards)
-
-    assert_equal(3, deck.count)
+    assert_equal(3, @deck.count)
   end
 end
