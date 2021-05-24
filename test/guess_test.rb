dbdd9fa103611cb85663450eae411bcd624ea8e7
Khoi Nguyen <khoinguyen311@gmail.com>
2021-05-23 17:05:26 -0700

    Add more guess testing

--- test/guess_test.rb
+++ test/guess_test.rb
@@ -10,4 +10,18 @@ class GuessTest < Minitest::Test
 
     assert_instance_of(Guess, guess)
   end
+
+  def test_it_has_a_card_to_guess
+    card = Card.new("10", "Hearts")
+    guess = Guess.new("10 of Hearts", card)
+
+    assert_instance_of(Card, guess.card)
+  end
+
+  def test_it_makes_a_guess
+    card = Card.new("10", "Hearts")
+    guess = Guess.new("10 of Hearts", card)
+
+    assert_equal("10 of Hearts", guess.guess)
+  end
 end
