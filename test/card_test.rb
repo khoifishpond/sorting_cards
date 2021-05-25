5cb77c993e9acbad7c24d1d90c593fe9756c7959
Khoi Nguyen <khoinguyen311@gmail.com>
2021-05-25 03:11:23 -0700

    Add has ranking tests

--- test/card_test.rb
+++ test/card_test.rb
@@ -17,4 +17,14 @@ class CardTest < Minitest::Test
     card = Card.new("Ace", "Spades")
     assert_equal "Spades", card.suit
   end
+
+  def test_it_has_a_rank
+    card = Card.new("Ace", "Spades")
+    assert_equal 14, card.rank
+  end
+
+  def test_suits_also_have_a_rank
+    card = Card.new("Ace", "Spades")
+    assert_equal(4, card.suit_rank)
+  end
 end
