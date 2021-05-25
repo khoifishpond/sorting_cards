1cd424d1c336aeeedf56715afa86618e77cac4e6
Khoi Nguyen <khoinguyen311@gmail.com>
2021-05-25 03:11:52 -0700

    Add sorting tests

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,10 +5,12 @@ require './lib/card'
 
 class DeckTest < Minitest::Test
   def setup
-    @card_1 = Card.new("3", "Hearts")
-    @card_2 = Card.new("4", "Clubs")
+    @card_1 = Card.new("4","Hearts")
+    @card_2 = Card.new("Ace", "Spades")
     @card_3 = Card.new("5", "Diamonds")
-    @cards = [@card_1, @card_2, @card_3]
+    @card_4 = Card.new("Jack", "Clubs")
+    @card_5 = Card.new("Ace", "Diamonds")
+    @cards = [@card_1, @card_2, @card_3, @card_4, @card_5]
 
     @deck = Deck.new(@cards)
   end
@@ -19,10 +21,18 @@ class DeckTest < Minitest::Test
 
   def test_it_takes_an_array_of_cards
     assert_instance_of(Array, @deck.cards)
-    assert_instance_of(Card, @deck.cards[0])
+    assert_instance_of(Card, @deck.cards.first)
   end
 
   def test_it_can_count_number_of_cards_in_deck
-    assert_equal(3, @deck.count)
+    assert_equal(5, @deck.count)
+  end
+
+  def test_it_can_sort_cards
+    @deck.sort
+    assert_equal(4, @deck.cards.first.rank)
+    assert_equal(14, @deck.cards.last.rank)
+    assert_equal("Diamonds", @deck.cards[3].suit)
+    assert_equal("Spades", @deck.cards.last.suit)
   end
 end
