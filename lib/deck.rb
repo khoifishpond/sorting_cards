038c4cca9cac04480b62070d1763c55c3aa71d92
Khoi Nguyen <khoinguyen311@gmail.com>
2021-05-25 03:11:01 -0700

    Add sort method using bubble sort

--- lib/deck.rb
+++ lib/deck.rb
@@ -8,4 +8,27 @@ class Deck
   def count
     @cards.size
   end
+
+  def sort
+    # A > K > Q > J > 10 > .. > 2
+    # spades > hearts > diamonds > clubs
+    # bubble sort
+    loop do
+      swapped = false
+
+      (count - 1).times do |index|
+        if @cards[index].rank == @cards[index + 1].rank
+          if @cards[index].suit_rank > @cards[index + 1].suit_rank
+            @cards[index], @cards[index + 1] = @cards[index + 1], @cards[index]
+            swapped = true
+          end
+        elsif @cards[index].rank > @cards[index + 1].rank
+          @cards[index], @cards[index + 1] = @cards[index + 1], @cards[index]
+          swapped = true
+          require 'pry'; binding.pry
+        end
+      end
+      break if not swapped
+    end
+  end
 end
