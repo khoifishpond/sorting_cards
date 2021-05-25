f84fe902b412c50f2c88a7537253fe0217a3ba7f
Khoi Nguyen <khoinguyen311@gmail.com>
2021-05-24 17:01:25 -0700

    Add record_guess and number_correct methods

--- lib/round.rb
+++ lib/round.rb
@@ -1,3 +1,6 @@
+require "./lib/guess"
+require "./lib/card"
+
 class Round
   attr_reader :deck, :guesses
 
@@ -8,5 +11,23 @@ class Round
 
   def current_card
     @deck.cards.first
+  end
+
+  def record_guess(guess)
+    card = current_card
+    @guesses << Guess.new("#{guess[:value]} of #{guess[:suit]}", card)
+    @deck.cards.shift
+
+    @guesses.last
+  end
+
+  def number_correct
+    correct_counter = 0
+    @guesses.each do |guess|
+      if guess.correct? == true
+        correct_counter += 1
+      end
+    end
+    correct_counter
   end
 end
